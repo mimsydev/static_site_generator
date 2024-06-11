37f635c49e78f16e527ae12bded799ac2772b8ef
Johnathan Rhoades <jrhoades7989@gmail.com>
2024-06-11 07:39:38 -0400

    added a text_node to leaf_node converter

---
+++ src/converttextnode.py
@@ -0,0 +1,39 @@
+from textnode import TextNode
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    valid_text_types = { \
+            #"type": ("tag", "self_closing", ("[v]alue[p]rops", ["prop1", "prop2", ...]))
+            "text": ("", False, ("v", None)), \
+            "bold": ("b", False, ("v", None)), \
+            "italic": ("i", False, ("v", None)), \
+            "code": ("code", False, ("v", None)), \
+            "link": ("a", True, ("vp", ["href"])), \
+            "image": ("img", True, ("p", ["alt", "src"])) \
+            }
+
+    if text_node.text_type not in valid_text_types.keys():
+        text_type = text_node.text_type
+        valid_types = "',".join(valid_text_types.keys())
+        raise ValueError(f"The text_type '{text_type}' is not supported. The accepted text_types are '{valid_types}'.")
+
+    text_info = valid_text_types[text_node.text_type]
+    tag = text_info[0] if len(text_info[0]) > 0 else None
+    self_closing = text_info[1]
+    format = text_info[2][0]
+    value = ""
+    props = {}
+
+    match format:
+        case "v":
+            value = text_node.text
+        case "p":
+            props[text_info[2][1][0]] = text_node.text
+            props[text_info[2][1][1]] = text_node.url
+        case "vp":
+            value = text_node.text
+            props[text_info[2][1][0]] = text_node.url
+        case _:
+            raise ValueError("The text type yielded an invalid value/attribute format")
+
+    return LeafNode(value, tag, props, self_closing)
---
+++ src/test_converttextnode.py
@@ -0,0 +1,36 @@
+import unittest
+from textnode import TextNode
+from converttextnode import text_node_to_html_node
+
+class TestConvertTextNode(unittest.TestCase):
+    def setUp(self):
+        self.text_type_text = TextNode("This is some raw text", "text")
+        self.text_type_bold = TextNode("This is some bold text", "bold")
+        self.text_type_italic = TextNode("This is some italic text", "italic")
+        self.text_type_code = TextNode("This is some code text", "code")
+        self.text_type_link = TextNode("This is a link","link", "https://localhost:8888")
+        self.text_type_image = TextNode("This is some alt text", "image", "https://localhost:8888")
+        self.expected_text = "This is some raw text"
+        self.expected_bold = "<b>This is some bold text</b>"
+        self.expected_italic = "<i>This is some italic text</i>"
+        self.expected_code = "<code>This is some code text</code>"
+        self.expected_link = "<a href=\"https://localhost:8888\">This is a link</a>"
+        self.expected_image = "<img alt=\"This is some alt text\" src=\"https://localhost:8888\">"
+
+    def test_text(self):
+        self.assertEqual(text_node_to_html_node(self.text_type_text).to_html(), self.expected_text)
+
+    def test_bold(self):
+        self.assertEqual(text_node_to_html_node(self.text_type_bold).to_html(), self.expected_bold)
+
+    def test_italic(self):
+        self.assertEqual(text_node_to_html_node(self.text_type_italic).to_html(), self.expected_italic)
+
+    def test_code(self):
+        self.assertEqual(text_node_to_html_node(self.text_type_code).to_html(), self.expected_code)
+
+    def test_link(self):
+        self.assertEqual(text_node_to_html_node(self.text_type_link).to_html(), self.expected_link)
+
+    def test_image(self):
+        self.assertEqual(text_node_to_html_node(self.text_type_image).to_html(), self.expected_image)
